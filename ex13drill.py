c836e463f23a01f5ab3e993a24fc2a78307c597d
Lori <lori.imbesi@gmail.com>
2020-01-16 22:30:51 -0500

    Parameters, Unpacking, Variables

---
+++ ex13drill.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+
+script, first, second, third, fourth = argv
+
+print("The script it called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+print("Your fourth variable is:", fourth)
+
+favcolor = input("What is your favorite color? ")
+
+print(f"So, your favorite color is: {favcolor}")
