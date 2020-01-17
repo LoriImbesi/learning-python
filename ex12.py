e8e819ab5eb2ff182d2599410e181c8b2fb1b5d8
Lori <lori.imbesi@gmail.com>
2020-01-16 22:29:28 -0500

    Prompting People

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+eyecolor = input("What is your eyecolor? ")
+
+print(f"So, you're {age} years old, {height} tall and your eyecolor is {eyecolor}.")
